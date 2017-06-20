cbe2b693c01ec7ffe42e8488456e9a79aea996b4
samfun123 <samdinner@hotmail.com>
2017-06-20 08:31:41 -0400

    Serial Number Change and Refactoring

--- Assets/Scripts/WidgetExpandService.cs
+++ Assets/Scripts/WidgetExpandService.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Reflection;
 using UnityEngine;
 
@@ -7,29 +8,47 @@ public class WidgetExpandService : MonoBehaviour
 {
     public class WidgetExpandModSettings
     {
+        public bool allowWidgetCountChange = true;
         public int minimumWidgetCount = 7;
         public int maximumWidgetCount = 7;
+        
+        public bool allowSerialNumberChange = true;
     }
 
     private WidgetExpandModSettings _modSettings = new WidgetExpandModSettings();
+    private KMModSettings modSettingsComponent;
 
     private Type _widgetGeneratorType = null;
     private FieldInfo _widgetCountField = null;
 
     private bool _refreshWidgetCount = true;
 
+    private Type _serialNumberType;
+    private FieldInfo _serialNumberArrayField;
+    private MethodInfo _serialNumberStartMethod;
+
+    private void DebugLog(object text, params object[] formatting)
+    {
+        Debug.LogFormat("[WidgetExpander] " + text, formatting);
+    }
+
+    private void ReadSettings()
+    {
+        _modSettings = JsonConvert.DeserializeObject<WidgetExpandModSettings>(modSettingsComponent.Settings);
+        DebugLog("Widget Expansion: {0}\nMinimum: {1}\nMaximum: {2}", _modSettings.allowWidgetCountChange ? "Enabled" : "Disabled", _modSettings.minimumWidgetCount, _modSettings.maximumWidgetCount);
+        DebugLog("Serial Number Change: ", _modSettings.allowSerialNumberChange ? "Enabled" : "Disabled");
+    }
+
     private void Start()
     {
         _widgetGeneratorType = ReflectionHelper.FindType("WidgetGenerator");
-        Debug.Log("Widget generator type = " + _widgetGeneratorType.ToString());
-
         _widgetCountField = _widgetGeneratorType.GetField("NumberToGenerate", BindingFlags.Instance | BindingFlags.Public);
-        Debug.Log("Widget count field = " + _widgetCountField.ToString());
 
-        KMModSettings modSettingsComponent = GetComponent<KMModSettings>();
-        Debug.Log("Widget expand settings = " + modSettingsComponent.Settings);
-        _modSettings = JsonConvert.DeserializeObject<WidgetExpandModSettings>(modSettingsComponent.Settings);
-        Debug.Log(string.Format("Widget expand settings: minimum = {0}, maximum = {1}", _modSettings.minimumWidgetCount, _modSettings.maximumWidgetCount));
+        modSettingsComponent = GetComponent<KMModSettings>();
+        
+        _serialNumberType = ReflectionHelper.FindType("SerialNumber");
+        _serialNumberArrayField = _serialNumberType.GetField("possibleCharArray", BindingFlags.NonPublic | BindingFlags.Instance);
+        _serialNumberStartMethod = _serialNumberType.GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
 
         KMGameInfo gameInfoComponent = GetComponent<KMGameInfo>();
         gameInfoComponent.OnStateChange += OnStateChange;
@@ -45,7 +64,13 @@ public class WidgetExpandService : MonoBehaviour
 
     private void OnStateChange(KMGameInfo.State state)
     {
-        _refreshWidgetCount = _refreshWidgetCount || state == KMGameInfo.State.Transitioning || state == KMGameInfo.State.Setup;
+        _refreshWidgetCount = _refreshWidgetCount || state == KMGameInfo.State.Gameplay;
+
+        if (_refreshWidgetCount && _modSettings.allowSerialNumberChange)
+        {
+            StartCoroutine(ReplaceSerialNumber());
+        }
+       
     }
 
     private void UpdateWidgetCount()
@@ -53,17 +78,39 @@ public class WidgetExpandService : MonoBehaviour
         UnityEngine.Object widgetGenerator = FindObjectOfType(_widgetGeneratorType);
         if (widgetGenerator == null)
         {
-            Debug.Log("Failed to find the widget generator object.");
             return;
         }
 
         //Because the rule manager resets the random seed to 1 before bomb generation
         UnityEngine.Random.InitState((int)Time.time);
 
+        ReadSettings();
+
         int widgetCount = UnityEngine.Random.Range(Mathf.Min(_modSettings.minimumWidgetCount, _modSettings.maximumWidgetCount), Mathf.Max(_modSettings.minimumWidgetCount, _modSettings.maximumWidgetCount) + 1);
-        _widgetCountField.SetValue(widgetGenerator, widgetCount);
-        Debug.Log(string.Format("Widget count set to {0}.", widgetCount));
+        _widgetCountField.SetValue(widgetGenerator, _modSettings.allowWidgetCountChange ? widgetCount : 5);
+        if (_modSettings.allowWidgetCountChange)
+        {
+            DebugLog("Widget count set to {0}.", widgetCount);
+        }
 
         _refreshWidgetCount = false;
+    }
+
+    IEnumerator ReplaceSerialNumber()
+    {
+        object serialNumber = null;
+        yield return new WaitUntil(() => { serialNumber = FindObjectOfType(_serialNumberType); return serialNumber != null; });
+
+        DebugLog("Replacing serial number...");
+
+        _serialNumberArrayField.SetValue(serialNumber, new char[]
+            {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+            }
+        );
+
+        _serialNumberStartMethod.Invoke(serialNumber, null);
     }
 }
