fa5a78dd12b1332d0ba84dc30d5537303e1cd8c1
caitsith2 <d_good@caitsith2.com>
2017-06-20 08:59:35 -0700

    Multiple things
    * Added some debugging info as to what random indicators were chosen.
    * Refresh the persistent settings. Closes issue #2
    * Custom indicator count is now never below the maximum widget count,
    unless max widget count is more than 17576.

--- Assets/Scripts/WidgetExpandService.cs
+++ Assets/Scripts/WidgetExpandService.cs
@@ -2,15 +2,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using UnityEngine;
 
 public class WidgetExpandService : MonoBehaviour
 {
+    private static int SettingsVersion = 1;
     public class WidgetExpandModSettings
     {
+        public int fileVersion;
         public int minimumWidgetCount = 7;
         public int maximumWidgetCount = 7;
+        public bool enableCustomIndicators = true;
     }
 
     private WidgetExpandModSettings _modSettings = new WidgetExpandModSettings();
@@ -21,6 +25,8 @@ public class WidgetExpandService : MonoBehaviour
     private Type _indicatorWidgetType = null;
     private FieldInfo _indicatorLabelsField = null;
 
+    private List<string> _knownIndicators = null;
+    private List<string> _customIndicators = null;
     private bool _refreshWidgetCount = true;
 
     private void Start()
@@ -37,10 +43,22 @@ public class WidgetExpandService : MonoBehaviour
         _indicatorLabelsField = _indicatorWidgetType.GetField("Labels", BindingFlags.Public | BindingFlags.Static);
         Debug.Log("Indicator labels field = " + _indicatorLabelsField.ToString());
 
+        _knownIndicators = (List<string>) _indicatorLabelsField.GetValue(null);
+        string indicators = "";
+        foreach (string label in _knownIndicators)
+        {
+            if (indicators == "")
+                indicators = label;
+            else
+                indicators += ", " + label;
+        }
+        Debug.LogFormat("The Following Indicators are present: {0}", indicators);
+
         KMModSettings modSettingsComponent = GetComponent<KMModSettings>();
         Debug.Log("Widget expand settings = " + modSettingsComponent.Settings);
         _modSettings = JsonConvert.DeserializeObject<WidgetExpandModSettings>(modSettingsComponent.Settings);
         Debug.Log(string.Format("Widget expand settings: minimum = {0}, maximum = {1}", _modSettings.minimumWidgetCount, _modSettings.maximumWidgetCount));
+        Debug.Log(string.Format("Enable custom indicators: {0}", _modSettings.enableCustomIndicators));
 
         KMGameInfo gameInfoComponent = GetComponent<KMGameInfo>();
         gameInfoComponent.OnStateChange += OnStateChange;
@@ -59,37 +77,87 @@ public class WidgetExpandService : MonoBehaviour
         _refreshWidgetCount = _refreshWidgetCount || state == KMGameInfo.State.Transitioning || state == KMGameInfo.State.Setup;
     }
 
-    private List<string> MakeList(int count)
+    private void RefreshSettings()
     {
-        List<string> labels = new List<string>
+        string ModSettings = Path.Combine(Path.Combine(Application.persistentDataPath, "Modsettings"), "WidgetExpand-settings.txt");
+        if (File.Exists(ModSettings))
         {
-            "SND",
-            "CLR",
-            "CAR",
-            "IND",
-            "FRQ",
-            "SIG",
-            "NSA",
-            "MSA",
-            "TRN",
-            "BOB",
-            "FRK",
-            "NLL"
-        };
-        string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Debug.Log("Attempting to Refresh the settings");
+            string settings = File.ReadAllText(ModSettings);
+            _modSettings = JsonConvert.DeserializeObject<WidgetExpandModSettings>(settings);
 
+            if (_modSettings.fileVersion != SettingsVersion)
+            {
+                Debug.Log("Settings Version changed - Adding new Settings");
+                _modSettings.fileVersion = SettingsVersion;
+                settings = JsonConvert.SerializeObject(_modSettings, Formatting.Indented);
+                File.WriteAllText(ModSettings, settings);
+                Debug.LogFormat("New settings = {0}", settings);
+            }
+        }
+    }
+
+    private void Shuffle()
+    {
+        if (_customIndicators == null)
+        {
+            string _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            _customIndicators = new List<string>();
+
+            foreach (var x in _letters)
+                foreach (var y in _letters)
+                    foreach (var z in _letters)
+                        _customIndicators.Add(x.ToString() + y + z);
+
+            foreach (var x in _knownIndicators)
+                _customIndicators.Remove(x);
+            _customIndicators.Remove("NLL");
+        }
+
+        var list = _customIndicators;
+        int n = _customIndicators.Count;
+        while (n-- > 0)
+        {
+            int k = UnityEngine.Random.Range(0, n+1);
+            string value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+    private List<string> MakeList()
+    {
+        Shuffle();
+
+        int count = Math.Max(_modSettings.maximumWidgetCount - 12, (_modSettings.minimumWidgetCount + 1) / 2);
+        count = Math.Min(count, _customIndicators.Count);
+
+        List<string> labels = new List<string>(_knownIndicators);
+        if (!_modSettings.enableCustomIndicators)
+            return labels;
+
+        labels.Add("NLL");
+
+
+        Debug.Log("In Addition to the standard 11 Indicators as well as NLL");
+        Debug.LogFormat("The following {0} may spawn on the upcoming bomb(s)",count);
+
+        
+        string indicators = "";
         for (var i = 0; i < count; i++)
         {
-            
-            string label;
-            do
+            if (indicators == "")
+                indicators = _customIndicators[i];
+            else
+                indicators += ", " + _customIndicators[i];
+            if ((i % 16) == 15)
             {
-                label = letters[UnityEngine.Random.Range(0, 26)].ToString();
-                label += letters[UnityEngine.Random.Range(0, 26)].ToString();
-                label += letters[UnityEngine.Random.Range(0, 26)].ToString();
-            } while (labels.Contains(label));
-            labels.Add(label);
+                Debug.Log(indicators);
+                indicators = "";
+            }
+            labels.Add(_customIndicators[i]);
         }
+        Debug.Log(indicators);
 
         return labels;
     }
@@ -103,6 +171,9 @@ public class WidgetExpandService : MonoBehaviour
             return;
         }
 
+        //Get Fresh settings to apply to the next bomb
+        RefreshSettings();
+
         //Because the rule manager resets the random seed to 1 before bomb generation
         UnityEngine.Random.InitState((int)Time.time);
 
@@ -110,9 +181,9 @@ public class WidgetExpandService : MonoBehaviour
         _widgetCountField.SetValue(widgetGenerator, widgetCount);
         Debug.Log(string.Format("Widget count set to {0}.", widgetCount));
 
-        List<string> list = MakeList((_modSettings.minimumWidgetCount + _modSettings.maximumWidgetCount + 1) / 2);
+        List<string> list = MakeList();
         _indicatorLabelsField.SetValue(null, list);
-
+        
         _refreshWidgetCount = false;
     }
 }
